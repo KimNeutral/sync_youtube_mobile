8aa1fc22f45ab55c00ea403376a14fa68b4e884e
KimDaeyong <01rlaeodyd@naver.com>
2018-11-09 19:20:18 +0900

    add: added android native code to launch youtube.

--- android/app/src/main/java/com/neutral/syncyoutubemobile/MainActivity.java
+++ android/app/src/main/java/com/neutral/syncyoutubemobile/MainActivity.java
@@ -1,13 +1,40 @@
 package com.neutral.syncyoutubemobile;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    private static final String CHANNEL = "com.kimneutral.io/intent";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+
+        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
+            new MethodChannel.MethodCallHandler() {
+                @Override
+                public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+                    if (call.method.equals("launchYoutube")) {
+                        String url = call.argument("url");
+                        launchYoutube(url);
+                        result.success(null);
+                    } else {
+                        result.notImplemented();
+                    }
+                }
+            }
+        );
+    }
+
+    private void launchYoutube(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
 }
