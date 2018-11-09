c48869cf03427d1ae1fbdd040cec575c71662b02
KimDaeyong <01rlaeodyd@naver.com>
2018-11-09 19:17:28 +0900

    add: added TokenInfo model.

--- lib/model/token_info.dart
+++ lib/model/token_info.dart
@@ -5,4 +5,24 @@ class TokenInfo {
   String refreshToken;
 
   String getTokenWithPrefix() => PREFIX + token;
+
+
+  TokenInfo({this.token, this.refreshToken});
+
+  factory TokenInfo.fromJson(Map<String, dynamic> json) {
+    return TokenInfo(
+      token: json['token'],
+        refreshToken: json['refreshToken']
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'token': token,
+    'refreshToken': refreshToken
+  };
+
+  @override
+  String toString() {
+    return 'TokenInfo{token: $token, refreshToken: $refreshToken}';
+  }
 }
