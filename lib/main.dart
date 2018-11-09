9e798ba3975e37ea81389a60662383ac051e8f6d
KimDaeyong <01rlaeodyd@naver.com>
2018-11-09 19:24:32 +0900

    modify: modified main ui.

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,161 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sync_youtube_mobile/model/token_info.dart';
+import 'package:sync_youtube_mobile/model/youtube.dart';
+import 'package:sync_youtube_mobile/network/network_manager.dart';
+import 'dart:async';
 
-void main() => runApp(new MyApp());
+import 'package:stack_trace/stack_trace.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+Type typeOf<T>() => T;
+
+TokenInfo tokenInfo;
+
+const String CHANNEL = "com.kimneutral.io/intent";
+
+void main() async {
+  Type type = typeOf<List<Youtube>>();
+  var manager = NetworkManager();
+  print(type);
+  var resp = await manager.login("01rlaeodyd@naver.com", "1234");
+  if(resp.data != null) {
+    tokenInfo = resp.data;
+  }
+
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MainPage(title: 'Sync Youtube'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MainPageState createState() => new _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainPageState extends State<MainPage> {
+  static const platform = const MethodChannel(CHANNEL);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<List<Youtube>> youtube;
+
+  Future<List<Youtube>> loadAllYoutube() async {
+    var resp = await NetworkManager().getAllYoutube(tokenInfo);
+    return resp.data;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    youtube = loadAllYoutube();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var futureBuilder = FutureBuilder(
+      future: youtube,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return getMainWidget(context, snapshot);
+        } else if (snapshot.hasError) {
+          return Center(child: Text("Error occured!"));
+        }
+        return CircularProgressIndicator();
+      });
+
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      appBar: PreferredSize(
+        child: AppBar(
+          title: Text(widget.title, style: TextStyle(color: Colors.black),),
+          backgroundColor: Colors.white,
+          elevation: 2.0,
         ),
+        preferredSize: Size.fromHeight(50.0),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(
+        child: futureBuilder
+      ),
+    );
+  }
+
+  Widget getMainWidget(BuildContext context, AsyncSnapshot snapshot) {
+    if(snapshot.data.length == 0) {
+      return Text("No youtube data!");
+    } else {
+      return getListView(context, snapshot);
+    }
+  }
+
+  ListView getListView(BuildContext context, AsyncSnapshot snapshot) {
+    var list = snapshot.data as List<Youtube>;
+
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        var item = list[index];
+        print(item);
+        return embedDivider(
+          InkWell(
+            child: Container(
+              height: 85.0,
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+              child: Row(
+                children: <Widget>[
+                  getThumbnail(item.thumbnail),
+                  Padding(padding: EdgeInsets.only(right: 20.0)),
+                  Flexible(
+                    fit: FlexFit.loose,
+                    child: Text(
+                      item.title,
+                      overflow: TextOverflow.fade,
+                    )
+                  ),
+                ],
+              )
+            ),
+            onTap: () {
+              platform.invokeMethod("launchYoutube", <String,dynamic> {
+                "url": item.url
+              });
+            },
+          )
+        );
+      }
+    );
+  }
+
+  Widget embedDivider(Widget child) {
+    var children = <Widget>[
+      child,
+      Divider(height: 1.0, color: Colors.black38,),
+    ];
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: children,
+    );
+  }
+
+  Widget getThumbnail(url) {
+    return new CachedNetworkImage(
+      imageUrl: url,
+      placeholder: new CircularProgressIndicator(),
+      errorWidget: new Icon(Icons.error),
+      width: 100.0,
     );
   }
 }
